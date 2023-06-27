9e4d9592a4ebc45803645c6608f5e43fd5b7d8a2
SmeraldaKa0s <anabella.lerner@gmail.com>
2023-06-27 00:11:40 -0300

    agrego objetives

--- src/pages/course/[courseId].js
+++ src/pages/course/[courseId].js
@@ -13,14 +13,14 @@ export const getServerSideProps = async (context) => {
     const courseId = context.params.courseId;
     let courseData = {};
     let objetoNuevo = {};
-    
+
     try {
         const response = await axios.get(`${FRONTEND_URL}/courses_files/${courseId}/imsmanifest.xml`)
         const data = response.data;
-        
+
         objetoNuevo = JSON.parse(buildFromXML(data));
         //courseData = buildViejo(data);
-        
+
     }
     catch (error) {
         console.error('Error:', error);
@@ -32,17 +32,18 @@ export const getServerSideProps = async (context) => {
 }
 
 
-export default function Course({ courseId, courseData}) {
+export default function Course({ courseId, courseData }) {
     const [resource, setResource] = useState('#');
     const [lessonStatus, setLessonStatus] = useState("");
     const [lessonLocation, setLessonLocation] = useState("");
     const [sessionTime, setSessionTime] = useState("");
     const [totalTime, setTotalTime] = useState("");
+    const [objetives, setObjetives] = useState("");
     const [items, setItems] = useState([]);
     const [title, setTitle] = useState("");
 
 
-   console.log(courseData)
+    //console.log(courseData)
 
     const buildCourseUrl = (url) => `/courses_files/${courseId}/${url}`;
 
@@ -60,30 +61,30 @@ export default function Course({ courseId, courseData}) {
             </>
         ));
     }
-    
+
     useEffect(() => {
         const organizations = courseData.organizations;
-        const organization = organizations.organization; 
-        const firstOrganization = organization[0]; 
+        const organization = organizations.organization;
+        const firstOrganization = organization[0];
         const courseTitle = firstOrganization.title;
         const firstOrganizationItems = firstOrganization.items;
         const firstItemFromFirstOrg = firstOrganizationItems[0];
-        const resourceIdOfFirstItem = firstItemFromFirstOrg.identifierref; 
-       // console.log(firstItemFromFirstOrg, "FIRRRRRRRRRRRTS")
+        const resourceIdOfFirstItem = firstItemFromFirstOrg.identifierref;
+        // //console.log(firstItemFromFirstOrg, "FIRRRRRRRRRRRTS")
         setTitle(courseTitle)
-       setItems(firstOrganizationItems)
+        setItems(firstOrganizationItems)
 
         const resources = courseData.resources;
         const resource = resources.resource;
         const openingResource = resource.filter((r) => r.identifier === resourceIdOfFirstItem)[0]  //pueden haber varios resource 
-        console.log("RESOURCEEEEE", openingResource)
+        //console.log("RESOURCEEEEE", openingResource)
         const firsResourceUrl = openingResource.href;
         setResource(buildCourseUrl(firsResourceUrl))
     }, [])
 
     useEffect(() => {
         if (typeof window !== "undefined") {
-            window.alert = () => {}
+            window.alert = () => { }
             // Cuando existe el window configuramos scorm-again
             window.API = new Scorm12API({
                 autocommit: true,
@@ -96,33 +97,33 @@ export default function Course({ courseId, courseData}) {
             const usersCMI = localStorage.getItem("cmi");
             if (usersCMI) window.API.loadFromJSON(JSON.parse(usersCMI).cmi);
             window.API.on('LMSInitialize', (...rest) => {
-                console.log('Inicializado el LMS', rest)
+                //console.log('Inicializado el LMS', rest)
             });
             window.API.on('LMSGetLastError', (...rest) => {
-                console.log('LMSGetLastError', rest)
+                //console.log('LMSGetLastError', rest)
             });
             window.API.on('LMSGetErrorString', (...rest) => {
-                console.log('LMSGetErrorString', rest)
+                //console.log('LMSGetErrorString', rest)
             });
             window.API.on('LMSGetDiagnostic', (...rest) => {
-                console.log('LMSGetDiagnostic', rest)
+                //console.log('LMSGetDiagnostic', rest)
             });
             // Escucha el evento 'LMSSetValue.cmi.*'
             window.API.on('LMSSetValue.cmi.*', function (CMIElement, value) {
-                console.log('CMIelement', CMIElement);
+                //console.log('CMIelement', CMIElement);
                 window.API.storeData(true);
                 localStorage.setItem('cmi', JSON.stringify(window.API.renderCommitCMI(true)));
-                console.log(window.API.renderCommitCMI(true));
+                //console.log(window.API.renderCommitCMI(true));
 
                 // Comprueba si hay algún error
-              /*   const errorCode = window.API.LMSGetLastError();
-                if (errorCode !== "0") {
-                    const errorString = window.API.LMSGetErrorString(errorCode);
-                    const diagnostic = window.API.LMSGetDiagnostic(errorCode);
-                    console.error("SCORM Error: " + errorString + " Diagnostic: " + diagnostic);
-                } else {
-                    console.log("No SCORM error occurred");
-                } */
+                /*   const errorCode = window.API.LMSGetLastError();
+                  if (errorCode !== "0") {
+                      const errorString = window.API.LMSGetErrorString(errorCode);
+                      const diagnostic = window.API.LMSGetDiagnostic(errorCode);
+                      console.error("SCORM Error: " + errorString + " Diagnostic: " + diagnostic);
+                  } else {
+                      //console.log("No SCORM error occurred");
+                  } */
 
             });
 
@@ -132,27 +133,64 @@ export default function Course({ courseId, courseData}) {
 
             window.API.on('LMSSetValue.cmi.core.lesson_status', function (CMIElement, value) {
                 setLessonStatus(value);
-                console.log("Lesson status: " + value);
+                //console.log("Lesson status: " + value);
             });
 
             window.API.on('LMSSetValue.cmi.core.lesson_location', function (CMIElement, value) {
                 setLessonLocation(value);
-                console.log("Lesson location: " + value);
+                //console.log("Lesson location: " + value);
             });
 
             window.API.on('LMSSetValue.cmi.core.session_time', function (CMIElement, value) {
                 setSessionTime(value);
-                console.log("Session time: " + value);
+                //console.log("Session time: " + value);
             })
+
+            // Obtener el estado inicial del objetivo
+            let initialObjectiveStatus = window.API.LMSGetValue("cmi.objectives");
+            //console.log("Initial objective status: " + initialObjectiveStatus);
+            setObjetives(initialObjectiveStatus);
+
+            window.API.on('LMSSetValue.cmi.objectives.*', function (CMIElement, value) {
+                //console.log("Objective status: " + value);
+                const cmiPath = CMIElement.split('.');
+                const key = cmiPath[cmiPath.length - 1]
+                console.log('path', cmiPath)
+                setObjetives(obj => ({ ...obj, [key]: value}));
+                window.API.storeData(true);
+                //console.log(window.API.renderCommitCMI(true));
+            });
+
         }
+    }, [])
+
+    function setInitialObjective(objectiveIndex, id, minScore, maxScore, rawScore, status) {
+        // Para que funcione tiene que existir window.API
+        if(!window || !window.API) console.log("setInitialObjetive debe estar en el useEffect")
+        window.API.LMSSetValue("cmi.objectives." + objectiveIndex + ".id", id);
+        window.API.LMSSetValue("cmi.objectives." + objectiveIndex + ".score.min", minScore);
+        window.API.LMSSetValue("cmi.objectives." + objectiveIndex + ".score.max", maxScore);
+        window.API.LMSSetValue("cmi.objectives." + objectiveIndex + ".score.raw", rawScore);
+        window.API.LMSSetValue("cmi.objectives." + objectiveIndex + ".status", status);
+    }
+
+    
+    useEffect(() => {
+        window.API.on('LMSSetValue.cmi.objectives.*', function (CMIElement, value) {
+            console.log({CMIElement, value})
+        })
+        setTimeout(() => {
+            // Establecer un objetivo inicial
+            setInitialObjective(0, "Objetivo-nuevo", 0, 100, 0, "No te juno");
+        }, 1000)
     }, [])
 
     useEffect(() => {
         if (typeof window !== "undefined") {
             window.API.on('LMSCommit', function () {
-                console.log("LMSCommit has been called");
+                //console.log("LMSCommit has been called");
                 let value = window.API.LMSGetValue("cmi.core.total_time");
-                console.log("Total time: ", value);
+                //console.log("Total time: ", value);
                 setTotalTime(value);
             });
         }
@@ -169,7 +207,7 @@ export default function Course({ courseId, courseData}) {
                 <a href='/' className='my-2 px-3 h-3 py-1 rounded bg-slate-50 hover:bg-slate-500 hover:text-slate-50'>Back</a>
                 <h1 className='text-4xl font-bold my-6'>{title}</h1>
 
-               <div className='flex flex-col'>{!!items.length && !!items[0].items.length && renderItems(items) }</div> 
+                <div className='flex flex-col'>{!!items.length && !!items[0].items.length && renderItems(items)}</div>
 
                 <div className='mt-4 bg-blue-50'>
                     <h3 className='text-lg font-semibold'>SCORM Data:</h3>
@@ -177,6 +215,7 @@ export default function Course({ courseId, courseData}) {
                     <p>Lesson Location: {lessonLocation}</p>
                     <p>Session Time: {sessionTime}</p>
                     <p>Total Time: {totalTime}</p>
+                    <pre>Objectives: {JSON.stringify(objetives, null, 2)}</pre>
                 </div>
 
 
